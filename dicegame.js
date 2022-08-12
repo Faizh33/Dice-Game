21e5d50c8f5ac6d917c53ded215ce3b3da58a151
Erika REDON <erika_redon@hotmail.com>
2022-08-12 10:44:54 +0200

    create class players

--- dicegame.js
+++ dicegame.js
@@ -1,13 +1,33 @@
+// Create players class
+class players {
+    constructor(name, currentScore, totalScore, tour, winner) {
+        this.name = name
+        this.currentScore = currentScore
+        this.totalScore = totalScore
+        this.tour = false
+        this.winner = false
+    }
+}
 
+// Create each player
+let player1 = new players('player1', 0, 0, false, false);
+let player2 = new players('player2', 0, 0, false, false);
 let players = [player1, player2];
-let score;
-let points;
 
+let currentPlayer;
+let diceGameTour = 0;
+
+// Function Roll the Dice button 
 function rollTheDice() {
     setTimeout(function () {
-        var randomNumber = Math.floor(Math.random() * 6) + 1;
+        var currentScore = Math.floor(Math.random() * 6) + 1;
 
         document.querySelector(".img").setAttribute("src",
-            "./img-dice/dice" + randomNumber + ".png");
+            "./img-dice/dice" + currentScore + ".png");
     }
 )}
+
+//Function reset
+function newGame() {
+    document.querySelector("body").reset();
+}
