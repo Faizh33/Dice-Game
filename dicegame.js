e39e57d7c26434b63113453b4e62090a45dd3607
Erika REDON <erika_redon@hotmail.com>
2022-10-26 10:54:24 +0200

    roll the dice

--- dicegame.js
+++ dicegame.js
@@ -1,3 +1,6 @@
+let rollBtn = document.getElementById('rollDiceBtn');
+
+
 
 //Create players class
 class Players {
@@ -11,6 +14,33 @@ class Players {
 }  
 
 //Create each player
-let player1 = new Players(player1, 0, 0, false, false);
-let player2 = new Players(player2, 0, 0, false, false);
+let player1 = new Players('player1', 0, 0, false, false);
+let player2 = new Players('player2', 0, 0, false, false);
 
+//Function random number
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+//To roll the dice
+let rollTheDice = debounce(function (e) {
+        var randomNumber = random(1, 7);
+
+        document.querySelector(".img").setAttribute("src", "./img-dice/" + "dice" + randomNumber + ".png");
+        }, 500)
+
+//Fonction debounce
+function debounce(callback, delay) {
+  var timer;
+  return function () {
+    var args = arguments;
+    var context = this;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      callback.apply(context, args);
+    }, delay)
+  }
+}
+
+//event roll the dice
+rollBtn.addEventListener('click', rollTheDice);
