532083089bef655dddb2ec14bfb584596021c182
Erika REDON <erika_redon@hotmail.com>
2022-10-26 11:12:44 +0200

    start game and add current score

--- dicegame.js
+++ dicegame.js
@@ -1,4 +1,5 @@
 let rollBtn = document.getElementById('rollDiceBtn');
+let current = document.getElementsByClassName('currentScore')
 
 
 
@@ -17,6 +18,12 @@ class Players {
 let player1 = new Players('player1', 0, 0, false, false);
 let player2 = new Players('player2', 0, 0, false, false);
 
+//Start Game
+let gameTurn = 0;
+
+//Initialize active player
+let activePlayer;
+
 //Function random number
 function random(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -27,7 +34,13 @@ let rollTheDice = debounce(function (e) {
         var randomNumber = random(1, 7);
 
         document.querySelector(".img").setAttribute("src", "./img-dice/" + "dice" + randomNumber + ".png");
-        }, 500)
+
+        if(gameTurn === 0) {
+            player1.turn = true;
+            activePlayer = player1;
+            activePlayer.currentScore += randomNumber;
+            addCurrentScore()
+        }
 
 //Fonction debounce
 function debounce(callback, delay) {
@@ -42,5 +55,15 @@ function debounce(callback, delay) {
   }
 }
 
-//event roll the dice
+//Event roll the dice
 rollBtn.addEventListener('click', rollTheDice);
+
+//Function add current score
+function addCurrentScore() {
+  if (activePlayer = player1) {
+    current[0].textContent = player1.currentScore
+  } else {
+    current[1].textContent = player2.currentScore
+  }
+}
+
